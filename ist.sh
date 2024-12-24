d21ff4dc214b344a7f49a738253acdbf202c1fb6
Jaewoo Joung <89329303+JaewooJoung@users.noreply.github.com>
2024-12-24 07:20:45 +0100

    Update ist.sh

--- ist.sh
+++ ist.sh
@@ -5,8 +5,18 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# Disk setup
+# Fixed disk setup
 DEVICE="/dev/nvme0n1"
+EFI_PART="${DEVICE}p1"
+SWAP_PART="${DEVICE}p2"
+ROOT_PART="${DEVICE}p3"
+
+# Fixed credentials
+USERNAME="crux"
+USER_PASSWORD="1234"
+ROOT_PASSWORD="1234"
+
+# Set partition variables
 EFI_PART="${DEVICE}p1"
 SWAP_PART="${DEVICE}p2"
 ROOT_PART="${DEVICE}p3"
@@ -29,9 +39,9 @@ sgdisk -Z ${DEVICE}
 sgdisk -o ${DEVICE}
 
 # Create partitions with specific partition type GUIDs
-sgdisk -n 1:0:+1G -t 1:ef00 -c 1:"EFI System Partition" ${DEVICE}    # Larger EFI partition
-sgdisk -n 2:0:+8G -t 2:8200 -c 2:"Linux swap" ${DEVICE}             # Swap partition
-sgdisk -n 3:0:0 -t 3:8300 -c 3:"Linux root" ${DEVICE}               # Root partition
+sgdisk -n 1:0:+1G -t 1:ef00 -c 1:"EFI System Partition" ${DEVICE}
+sgdisk -n 2:0:+8G -t 2:8200 -c 2:"Linux swap" ${DEVICE}
+sgdisk -n 3:0:0 -t 3:8300 -c 3:"Linux root" ${DEVICE}
 
 # Wait for kernel to update partition table
 sleep 3
@@ -47,32 +57,44 @@ mkfs.ext4 ${ROOT_PART}
 # Mount partitions
 echo "Mounting partitions..."
 mount ${ROOT_PART} /mnt
-mkdir -p /mnt/boot/efi
-mount ${EFI_PART} /mnt/boot/efi
+mkdir -p /mnt/boot
+mount ${EFI_PART} /mnt/boot
 swapon ${SWAP_PART}
 
-# Install essential packages (removed grub)
+# Install essential packages
 echo "Installing base system..."
-pacstrap -K /mnt base linux linux-firmware \
-    base-devel intel-ucode rust \
+pacstrap -K /mnt \
+    base linux linux-firmware \
+    base-devel intel-ucode \
     networkmanager vim efibootmgr \
-    xorg plasma plasma-desktop sddm \
-    firefox konsole dolphin \
-    sudo dosfstools mtools os-prober \
+    man-db man-pages texinfo \
+    xorg plasma plasma-desktop plasma-wayland-session \
+    plasma-pa plasma-nm plasma-workspace \
+    kde-applications kio-extras \
+    dolphin konsole kate \
+    firefox \
+    sudo dosfstools mtools \
     noto-fonts-cjk adobe-source-han-sans-kr-fonts ttf-baekmuk \
-    gtk3 gtk2 qt5-base qt4 qt5-tools \
-    libappindicator-gtk3 libhangul anthy \
-    git automake autoconf libtool pkg-config
+    gtk3 gtk2 qt6-base qt5-base \
+    qt6-tools qt5-tools libappindicator-gtk3 \
+    libhangul anthy \
+    git automake autoconf libtool pkg-config \
+    zsh htop wget curl powerdevil \
+    discover packagekit-qt6 flatpak phonon-qt6-vlc \
+    plasma-integration xdg-desktop-portal-kde
 
 # Generate fstab
 echo "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Configure the system
-arch-chroot /mnt /bin/bash <<'CHROOT_COMMANDS'
+arch-chroot /mnt /bin/bash <<CHROOT_COMMANDS
 # Set timezone
 ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 hwclock --systohc
+
+# Enable time synchronization
+systemctl enable systemd-timesyncd
 
 # Set locale
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
@@ -82,27 +104,25 @@ locale-gen
 echo "LANG=ko_KR.UTF-8" > /etc/locale.conf
 
 # Set hostname
-echo "lisa" > /etc/hostname
+echo "arch" > /etc/hostname
 cat > /etc/hosts <<EOF
 127.0.0.1   localhost
 ::1         localhost
-127.0.1.1   lisa.localdomain lisa
+127.0.1.1   arch.localdomain arch
 EOF
 
-# Set root password
-echo "root:1234" | chpasswd
-
-# Create user
-useradd -m -G wheel -s /bin/bash crux
-echo "crux:1234" | chpasswd
+# Set passwords
+echo "root:${ROOT_PASSWORD}" | chpasswd
+useradd -m -G wheel -s /bin/bash ${USERNAME}
+echo "${USERNAME}:${USER_PASSWORD}" | chpasswd
 echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel
 
 # Install and configure systemd-boot
-bootctl --path=/boot/efi install
+bootctl --path=/boot install
 
 # Create boot loader configuration
-mkdir -p /boot/efi/loader/entries
-cat > /boot/efi/loader/loader.conf <<EOF
+mkdir -p /boot/loader/entries
+cat > /boot/loader/loader.conf <<EOF
 default arch
 timeout 3
 console-mode max
@@ -110,7 +130,7 @@ editor no
 EOF
 
 # Create arch boot entry
-cat > /boot/efi/loader/entries/arch.conf <<EOF
+cat > /boot/loader/entries/arch.conf <<EOF
 title   Arch Linux
 linux   /vmlinuz-linux
 initrd  /intel-ucode.img
@@ -122,48 +142,24 @@ EOF
 systemctl enable NetworkManager
 systemctl enable sddm
 
-# Generate initial ramdisk
-mkinitcpio -P
-
-# Install juliaup from AUR
-echo "Installing juliaup..."
-cd /tmp
-sudo -u ${USERNAME} git clone https://aur.archlinux.org/juliaup.git
-cd juliaup
-sudo -u ${USERNAME} makepkg -si --noconfirm
-cd /
-
-# Install Julia using juliaup (as user)
-echo "Setting up Julia..."
-sudo -u ${USERNAME} bash <<EOF
-juliaup add release
-juliaup default release
-EOF
-
-# Install Korean IME (nimf) and fonts
-echo "Installing Korean fonts and input method..."
-cd /tmp
-
 # Install Korean fonts from AUR
-FONT_PACKAGES=(
-    "spoqa-han-sans"
-    "ttf-d2coding"
-    "ttf-nanum"
-    "ttf-nanumgothic_coding"
-    "ttf-kopub"
-    "ttf-kopubworld"
-)
+cd /tmp
+sudo -u ${USERNAME} bash <<EOF
+git clone https://aur.archlinux.org/spoqa-han-sans.git
+cd spoqa-han-sans
+makepkg -si --noconfirm
+cd ..
 
-for font in "${FONT_PACKAGES[@]}"; do
-    echo "Installing $font..."
-    sudo -u ${USERNAME} git clone https://aur.archlinux.org/${font}.git
-    cd ${font}
-    sudo -u ${USERNAME} makepkg -si --noconfirm
+for font in ttf-d2coding ttf-nanum ttf-nanumgothic_coding ttf-kopub ttf-kopubworld; do
+    git clone https://aur.archlinux.org/\${font}.git
+    cd \${font}
+    makepkg -si --noconfirm
     cd ..
 done
+EOF
 
-# Install nimf from source
-echo "Installing nimf..."
+# Install nimf input method
+cd /tmp
 wget https://gitlab.com/nimf-i18n/nimf/-/archive/master/nimf-master.tar.gz
 tar xf nimf-master.tar.gz
 cd nimf-master
@@ -172,24 +168,10 @@ make
 make install
 ldconfig
 
-# Configure input method settings for the user
-sudo -u ${USERNAME} cat > /home/${USERNAME}/.xprofile <<EOF
-export GTK_IM_MODULE=nimf
-export QT4_IM_MODULE="nimf"
-export QT_IM_MODULE=nimf
-export XMODIFIERS="@im=nimf"
-nimf &
-EOF
-
-# Set proper permissions for the .xprofile
-chown ${USERNAME}:${USERNAME} /home/${USERNAME}/.xprofile
-
-# Update font cache
-fc-cache -fv
-
-# Create nimf autostart for the user
-sudo -u ${USERNAME} mkdir -p /home/${USERNAME}/.config/autostart
-sudo -u ${USERNAME} cat > /home/${USERNAME}/.config/autostart/nimf.desktop <<EOF
+# Configure input method for user
+sudo -u ${USERNAME} bash <<EOF
+mkdir -p /home/${USERNAME}/.config/autostart
+cat > /home/${USERNAME}/.config/autostart/nimf.desktop <<EOL
 [Desktop Entry]
 Type=Application
 Name=Nimf
@@ -200,25 +182,18 @@ Categories=System;
 X-GNOME-Autostart-Phase=Applications
 X-GNOME-AutoRestart=true
 X-GNOME-Autostart-enabled=true
+EOL
+
+cat > /home/${USERNAME}/.xprofile <<EOL
+export GTK_IM_MODULE=nimf
+export QT_IM_MODULE=nimf
+export XMODIFIERS="@im=nimf"
+nimf &
+EOL
 EOF
 
-# Create nimf configuration directory
-sudo -u ${USERNAME} mkdir -p /home/${USERNAME}/.config/nimf
-
-# Configure nimf settings
-sudo -u ${USERNAME} cat > /home/${USERNAME}/.config/nimf/nimf.conf <<EOF
-[Nimf]
-hidden-on-launch=false
-xkb-options=
-default-engine=nimf-system-keyboard
-default-locale=ko_KR.UTF-8
-gtk-im-module=nimf
-qt4-im-module=nimf
-qt5-im-module=nimf
-xim-name=nimf
-use-singleton=true
-EOF
-
+# Generate initial ramdisk
+mkinitcpio -P
 CHROOT_COMMANDS
 
 umount -R /mnt
@@ -228,15 +203,13 @@ echo ""
 echo "IMPORTANT POST-INSTALLATION STEPS:"
 echo "1. Power off the computer completely (not reboot)"
 echo "2. Remove the USB drive"
-echo "3. Enter BIOS setup and make these EXACT changes:"
+echo "3. Enter BIOS setup and make these changes:"
 echo "   a. Load BIOS defaults first"
 echo "   b. Disable Secure Boot"
 echo "   c. Set UEFI boot mode (disable CSM/Legacy completely)"
-echo "   d. Set Boot Device Priority:"
-echo "      - First: nvme0n1"
-echo "      - Disable or remove other boot options"
-echo "   e. Save changes and exit"
+echo "   d. Set Boot Device Priority to ${DEVICE}"
 echo ""
-echo "Login credentials:"
-echo "Username: crux"
-echo "Password: 1234"
+echo "After first boot:"
+echo "1. Korean input can be toggled with Shift+Space"
+echo "2. Run 'nimf-settings' to configure input method"
+echo "3. Use 'nimf --debug &' if you need to troubleshoot"
